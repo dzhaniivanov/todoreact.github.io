a0a37c80eaf3f0dd7faef86a0aca74f089c192ac
Dzhani Ivanov <djani.ivanov@abv.bg>
2021-03-05 20:50:32 +0200

    add todo,delete todo functionallity added

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,43 +1,73 @@
-import React from "react"
-import TodosList from './TodosList'
-import Header from './Header'
+import React from "react";
+import TodosList from './TodosList';
+import Header from './Header';
+import InputTodo from './InputTodo';
+import { v4 as uuidv4 } from "uuid";
 
 
 class TodoContainer extends React.Component {
     state = {
         todos: [
             {
-                id: 1,
+                id: uuidv4(),
                 title: 'Setup development environment',
                 completed: true
             },
             {
-                id: 2,
+                id: uuidv4(),
                 title: 'develop website and add content',
                 completed: false,
             },
             {
-                id: 3,
+                id: uuidv4(),
                 title: 'deploy to live server',
                 completed: false,
             },
         ]
     }
     handleChange = id => {
-        this.setState({
-            todos: this.state.todos.map(todo => {
+        this.setState(prevState => ({
+            todos: prevState.todos.map(todo => {
                 if (todo.id === id) {
-                    todo.completed = !todo.completed;
+                    return {
+                        ...todo,
+                        completed: !todo.completed,
+                    }
                 }
-                return todo;
-            })
+                return todo
+            }),
+        }))
+    };
+
+    deleteToDo = id => {
+        this.setState({
+            todos: [
+                ...this.state.todos.filter(todo => {
+                    return todo.id !== id;
+                })
+            ]
+        });
+    }
+    addTodoItem = title => {
+        const newTodo = {
+            id: uuidv4(),
+            title: title,
+            completed: false,
+        };
+        this.setState({
+            todos: [...this.state.todos, newTodo]
         });
     };
     render() {
         return (
             <div>
                 <Header />
-                <TodosList todos={this.state.todos} handleChangeProps={this.handleChange} />
+                <InputTodo addTodoProps={this.addTodoItem} />
+                <TodosList
+                    todos={this.state.todos}
+                    handleChangeProps={this.handleChange}
+                    deleteTodoProps={this.deleteToDo}
+                />
             </div>
         );
     }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,11 +4,18 @@ class TodoItem extends React.Component {
     render() {
         return (
             <li>
-                <input 
-                type="checkbox" 
-                checked={this.props.todo.completed}
-                onChange={()=>this.props.handleChangeProps(this.props.todo.id)} 
+                <input
+                    type="checkbox"
+                    checked={this.props.todo.completed}
+                    onChange={() => this.props.handleChangeProps(this.props.todo.id)}
+
                 />
+                {this.props.todo.title}
+
+
+                <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+                    delete
+                    </button>
             </li>
         )
     }
