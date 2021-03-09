02213544a6749284b65141c6b95e30ca7bf1a324
Dzhani Ivanov <djani.ivanov@abv.bg>
2021-03-09 16:18:02 +0200

    deploying

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
-//import { BrowserRouter as Router } from "react-router-dom"
-import {HashRouter as Router} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom"
+//import {HashRouter as Router} from "react-router-dom";
 
 
 import TodoContainer from './functionBased/components/TodoContainer'
@@ -10,7 +10,7 @@ import './functionBased/App.css'
 
 ReactDOM.render(
     <React.StrictMode>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
             <TodoContainer />
         </Router>
     </React.StrictMode>,
